a9d070c16b341be980591318ade0e6addb6207be
Lalibela-Dev <abeventss123@gmail.com>
2022-12-01 12:14:19 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-        if chr(i) != 'e' and chr(i) != 'q':
-            print('{:c}'.format(i), end='')
+for a in range(ord("a"), ord("z") + 1):
+        if chr(a) is not "q" and chr(a) is not "e":
+            print("{:s}".format(chr(a)), end="")
